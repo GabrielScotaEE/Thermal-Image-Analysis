9857c4ee9c3867714ac8b879d09c7a1a82db8ed3
Gabriel Scota Fernandes <84752275+GabrielScotaEE@users.noreply.github.com>
2021-08-20 17:56:34 -0300

    Improoved code and comented.

--- imgProcessor.py
+++ imgProcessor.py
@@ -24,20 +24,10 @@ class imgProcessor():
         self.voltage_list = [0,13000,15000,17000,19000,21000,23000]
 
         pass
-
-    def filter_hsv_colors(self,hsv_image):
-
-        hsv_color1 = np.asarray([0,0,0])   # white red
-        hsv_color2 = np.asarray([23,255,255])   # dark orange
-        hsv_color3 = np.asarray([150,0,0])   # white red/pink
-        hsv_color4 = np.asarray([180,255,255])    # darker red
-
-        mask1 = cv.inRange(hsv_image, hsv_color1, hsv_color2)
-        mask2 = cv.inRange(hsv_image, hsv_color3, hsv_color4)
-        mask = cv.bitwise_or(mask1,mask2)
-        return mask
     
-    def filterHSVThresholdConfigurable(self,hsv_image, lowerHSV: list, higherHSV: list, lowerHSV2= None, higherHSV2=None):
+    # This function creates a mask with only 
+    # the colors sent by the user in HSV domain.
+    def filterHSVspeficColors(self,hsv_image, lowerHSV: list, higherHSV: list, lowerHSV2= None, higherHSV2=None):
 
         hsv_color1 = np.asarray(lowerHSV)   
         hsv_color2 = np.asarray(higherHSV)   
@@ -50,11 +40,11 @@ class imgProcessor():
             hsv_color4 = np.asarray(higherHSV2)    # darker red
             mask2 = cv.inRange(hsv_image, hsv_color3, hsv_color4)
             mask = cv.bitwise_or(mask,mask2)
-            return mask
+            
 
         return mask
    
-    def find_edge_points(self,contours):
+    def find_edge_points(self,contours, count):
         list_coord_y =[]
         list_coord_x =[]
         
@@ -69,14 +59,18 @@ class imgProcessor():
         extTop = min(list_coord_y)
         extBot = max(list_coord_y)
 
-        
-        extTop = extTop-3
-        extLeft = extLeft-3
-        extBot = extBot+3
-        extRight = extRight + 3
+        if count >0:
+            extTop = extTop-3
+            extLeft = extLeft-3
+            extBot = extBot+3
+            extRight = extRight+3
 
         return extLeft, extRight, extTop, extBot
 
+    # This function returns a img only with the colors
+    # from the filterHSVspeficColors function.
+    # obs: Mask returned in filterHSVspeficColors, is a grayscale img.
+    # i.e this function converts to colorspaces.
     def build_coi(self, mask_img, orig_img):
 
         imask = mask_img>0
@@ -86,8 +80,7 @@ class imgProcessor():
         return rgb_coi, coi
 
     def show_images(self, rgb_crop_img, rgb_original_img, count):
-            
-        
+       
         fig, _ = plt.subplots(nrows=1, ncols=2)
         fig.canvas.manager.set_window_title('{}V'.format(self.voltage_list[count]))
         fig.tight_layout()
@@ -103,16 +96,17 @@ class imgProcessor():
         plt.imshow(rgb_img_cropped)
         pass
     
+    # This function gets the countours using 
+    # canny edge detector.
     def getContours(self,mask):
 
         canny = imutils.auto_canny(mask)
         contours, _ = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        
         contours = np.array(contours)
-
         return contours
 
-   
+    # Get croped COI(colors of interest image) that contains black pixels
+    # and ignore then. Being left with only colors pixels.
     def ignoreBlackPixels(self, croped_rgbCoiImg):
         # Using set because we only want unique values
         # And we are studying only max and min temperatures
@@ -121,36 +115,49 @@ class imgProcessor():
         rows = croped_rgbCoiImg.shape[0]
         columns = croped_rgbCoiImg.shape[1]
 
-        # Appending only pixels != [0,0,0] <---  black color
+        # Appending on ignoreblack set
+        # only pixels != [0,0,0] <---  black color
         for row in range(rows):
             for col in range(columns):
                 if list(croped_rgbCoiImg[row][col]) != [0, 0, 0]:
                     ignore_black.add(tuple(croped_rgbCoiImg[row][col]))
-        
+        # Convert back to list, since sets
+        # are unindexable
         ignore_black = list(ignore_black)                    
         
         return (ignore_black)
 
-   
+    # Calculate the temperatura through
+    # the similarity of two pixels (colors)
     def calcTemp(self, ignoredBlack, crop_colorbar, map=None):
         
         self.ignoredBlack = ignoredBlack
         self.crop_colorbar = crop_colorbar
         temperature = []
-        
+        # This for runs over all elements in ignoredBlack list
+        # i.e compare each pixel from then with the colorbar pixels.
+        # but if the user gives the map, the function just use the map
+        # instead comparing again every single pixel improoving the performance. 
         for self.element in range(len(ignoredBlack)):
             memo = None
            
             init = time.time()
+            # Get the RGB color from the img, and convert to string
+            # The reason to convert to string is because the map
+            # return it map.key() in string.
             imPixel = str(self.ignoredBlack[self.element])
-            
+            # Check if the users gives the map and check if
+            # this rgb value is on the map, if is not in there, 
+            # run over colorbar pixels to compare each other.
+            # If map is not given just compare every img pixel
+            # with the colorbar pixels.
             if map is not None:
                 if imPixel in map:
                     id = map[imPixel]
                 else:
                     id, memo = self.__compareTwoColors_similarity()
 
-            # Calculate the similarity of two collors
+            # Calculate the similarity of two collors(pixels)
             # one from the image, another from the colorbar    
             else:
                 id, memo = self.__compareTwoColors_similarity()
@@ -166,46 +173,67 @@ class imgProcessor():
         # using createCSV_withColorsAndIds function
         return temperature, memo
     
-    
+    # This functions run over all elements of the colorbar
+    # comparing the with the img pixels.
     def __compareTwoColors_similarity(self):
         similarityList = []
         
-        # Comparing all pixels from img with pixels in colorbar
-        # to see wich color is the nearest to it.
+        # Converting the RGB values from ignoredBlack to tuple
+        # to check if this value is in self.memo
         imgPixel_tuple = tuple(self.ignoredBlack[self.element])
-            
-        if imgPixel_tuple in self.memo:
-            return self.memo[imgPixel_tuple], self.memo
-        else: 
+        # self.memo is a responsible variable for memorizing
+        # unique colors with your respective id. So its function 
+        # is to improve performance
+        if imgPixel_tuple not in self.memo:
+            # Comparing all pixels from img with pixels in colorbar
+            # to see wich color is the nearest to it. 
+            # And also storing the RGB with the respective id
+            # in self.memo
             for index in range (self.crop_colorbar_columns):
-
+                # Getting the pixel from the img.
                 pixImg = np.asarray(self.ignoredBlack[self.element])
+                # Converting to sRGB
                 colorSRGB_img = sRGBColor(pixImg[0]/255, pixImg[1]/255, pixImg[2]/255)
-
+                # Getting the pixel from the colorbar.
                 pixColorBar = self.crop_colorbar[0][index]
+                # Converting to sRGB
                 colorSRGB_colorbar = sRGBColor(pixColorBar[0]/255, pixColorBar[1]/255, pixColorBar[2]/255)
                 
+                # The convertion from sRGB to Lab is made below
+                # because the delta_e_cie2000 only accept Lab colors.
+
                 # color1 refers to image
                 color1_lab = convert_color(colorSRGB_img, LabColor)
-
-                # # colors2 refers to colorbar
+                # colors2 refers to colorbar
                 color2_lab = convert_color(colorSRGB_colorbar, LabColor)
-                
+                # The convertion from sRGB to Lab is made 
+                # because the delta_e_cie2000 only accept Lab colors.
+
                 # Delta return a value, the lowest value means
                 # that color is the similarest
                 delta_e = delta_e_cie2000(color1_lab, color2_lab)
                 
+                # Here is another way to compare the similarity
+                # of two colors
                 # distance = (pixImg[0]-pixColorBar[0])**2+(pixImg[1]-pixColorBar[1])**2+(pixImg[2]-pixColorBar[2])**2
                 # distance = np.sqrt(distance)
 
+                # Append all values from delta result and store 
+                # in similarityList
                 similarityList.append(delta_e)
 
             # Get the lowest value from the similarity list
+            # the lowest value shows the most similar color
             id = similarityList.index(min(similarityList))
             
-            
+            # Saving this RGB value from the img 
+            # with it respective id
             self.memo[imgPixel_tuple]=id
             return id, self.memo
+        else:
+            id = self.memo[imgPixel_tuple]
+            return id, self.memo
+            
         
     def printLikeaTable(self, list_area_percent_roi, list_o2r_pixels, list_area_percent_total, voltage_list):
         
@@ -214,6 +242,8 @@ class imgProcessor():
         pixel_total_df = pd.DataFrame(list(zip(list_area_percent_total,voltage_list)), columns =['%','Voltage'])
         print(pixel_total_df)
 
+    # Creates CSV map with Colors and it respectives ids.
+    # with memo variable(dict), that stores the RGB values and id.
     def createCSV_withColorsAndIds(self, memo):
         data = []
         for key, value in memo.items():
@@ -232,6 +262,9 @@ class imgProcessor():
             # write multiple rows
             writer.writerows(data)
     
+    # Loads the map from a csv file
+    # note that the map.keys() are string values
+    # and map.values() are int values
     def loadMapCSV(self, CSVnameFile):
         with open(CSVnameFile, encoding="utf8") as f:
             csv_reader = csv.DictReader(f)
